6a8ef6903a89dfa11f1380e358260113c76d173d
Nick Romans <nicholas.romans@gmail.com>
2018-08-22 18:27:41 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -21,7 +21,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 end
 def valid_phone_number?(phone)
-if phone.match(/\d./)
+if phone.match(/[(]?\d{3}[),\s]?\d{3}[\W,\s]?\d{4}?/)
   true
 end
 
